d7ec701cb1619d64583051111fc243ee4a779f37
Theo Markovic <icanhasspamz@gmail.com>
2021-12-08 08:16:54 +0100

    Update day4.fsx

--- day4.fsx
+++ day4.fsx
@@ -1,6 +1,9 @@
 open System.IO
+open System.Text
 open System
 
+
+// Read data
 let input =
     @"day4.txt" |> File.ReadAllLines |> Array.toList
 
@@ -11,55 +14,58 @@ let numbers =
     |> Array.toList
     |> List.map int
 
-let checkWin ls = 
-                ls 
-                |> List.exists (fst >> (=) false)
-                |> not
-
-let checkBoard (b: 'a list list) =
-    let colWin = b |> List.transpose |> List.map checkWin |> List.exists (fun x -> x = true)
-    let rowWin = b |> List.map checkWin |> List.exists (fun x -> x = true)
-    rowWin || colWin
-
-let mapm fn m = m |> List.map (fun x -> List.map fn x ) 
-
-let marknumber n board =
-                    board
-                    |> mapm (fun (marked, x) -> if x = n then (true,x) else (marked,x))
-
 let boards =
     input
     |> List.tail
     |> List.filter (String.IsNullOrEmpty >> not)
-    |> List.map (fun x ->
-        x.Split(" ")
-        |> Array.filter (String.IsNullOrEmpty >> not)
-        |> Array.map (fun y -> (false,int y))
-        |> Array.toList
-        )
+    |> List.map
+        (fun x ->
+            x.Split(" ")
+            |> Array.filter (String.IsNullOrEmpty >> not)
+            |> Array.map (fun y -> (false, int y))
+            |> Array.toList)
     |> List.chunkBySize 5
 
-let rec correctBoards (ns: int list) (bs: (bool * int) list list list) =
-        match ns with
-        | [] -> (None,None)
-        | drawNumber::tail ->
-                let marked = bs |> List.map (marknumber drawNumber)
+let checkWin ls =
+    ls |> List.exists (fst >> (=) false) |> not
 
-                match ( marked |> List.tryFind (checkBoard)) with
-                | None -> correctBoards tail marked
-                | Some _ as res -> (res, Some drawNumber)
+let checkBoard (b: (bool * int) list list) =
+    let colWin =
+        b
+        |> List.transpose
+        |> List.map checkWin
+        |> List.exists (fun x -> x = true)
+
+    let rowWin =
+        b
+        |> List.map checkWin
+        |> List.exists (fun x -> x = true)
+
+    rowWin || colWin
+
+let mapm fn m = m |> List.map (fun x -> List.map fn x)
+
+let marknumber n board =
+    board
+    |> mapm (fun (marked, x) -> if x = n then (true, x) else (marked, x))
+
+let rec correctBoards (ns: int list) (bs: (bool * int) list list list) =
+    match ns with
+    | [] -> (None, None)
+    | drawNumber :: tail ->
+        let marked = bs |> List.map (marknumber drawNumber)
+
+        match (marked |> List.tryFind (checkBoard)) with
+        | None -> correctBoards tail marked
+        | Some _ as res -> (res, Some drawNumber)
 
 let correctedBoard = boards |> correctBoards numbers
 
-let unmarked = 
-        match fst correctedBoard with
-        | Some x -> List.concat x |> List.filter (fst >> (=) false) |> List.sumBy snd
-        | None -> 0
-let last = match snd correctedBoard with
-            | Some x -> x
-            | None -> 0
-
-
-let result = (unmarked * last)
-
-()
+let result res =
+    match correctedBoard with
+    | (Some board, Some drawNumber) ->
+        List.concat board
+        |> List.filter (fst >> (=) false)
+        |> List.sumBy snd
+        |> (*) drawNumber
+    | _ -> 0
